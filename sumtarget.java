82f1a77f02e3d9b33b91fb4680eeb540ea9663a2
ot-coder <ogboanthony315@gmail.com>
2024-10-19 21:25:59 +0200

    updates

---
+++ sumtarget.java
@@ -0,0 +1,47 @@
+
+ /* Definition for a binary tree node.
+ public class TreeNode {
+     int val;
+     TreeNode left;
+         TreeNode right;
+    TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+         this.left = left;
+          this.right = right;
+      }
+  }
+*/ 
+
+class sumtarget {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        Stack<TreeNode> node_stack = new Stack();
+        Stack<Integer> targetSum_stack = new Stack();
+
+        node_stack.add(root);
+        targetSum_stack.add(targetSum-root.val);
+
+        while (!node_stack.isEmpty()) {
+            TreeNode current_node = node_stack.pop();
+            int current_sum = targetSum_stack.pop();
+
+            if(current_node.left == null && current_node.right == null && current_sum == 0) {
+                return true;
+            }
+
+            if (current_node.left != null) {
+                node_stack.add(current_node.left);
+                targetSum_stack.add(current_sum - current_node.left.val);
+            }
+
+             if (current_node.right != null) {
+                node_stack.add(current_node.right);
+                targetSum_stack.add(current_sum - current_node.right.val);
+            }
+        }
+        return false;
+    }
+}

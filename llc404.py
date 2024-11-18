3df3364fb8132431649cf85a471719082761f2d2
ot-coder <ogboanthony315@gmail.com>
2024-11-18 19:35:39 +0100

    learned about the and not syntax in python

---
+++ llc404.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        def is_leaf(node):
+            return node and not node.left and not node.right
+
+        total = 0
+        if root.left and is_leaf(root.left):
+            total += root.left.val
+
+        total += self.sumOfLeftLeaves(root.left)
+        total += self.sumOfLeftLeaves(root.right)
+
+        return total

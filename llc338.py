a3ff48357f5ec89068ba83c2b92855839422aa91
ot-coder <ogboanthony315@gmail.com>
2024-12-15 19:44:55 +0100

    Dynamic programming implementation

---
+++ llc338.py
@@ -0,0 +1,12 @@
+#class Solution:
+#   def countBits(self, n: int) -> List[int]:
+#        return [bin(i).count('1') for i in range(n +1)]
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+
+        for i in range(1, n+1):
+            ans[i] = ans[i >> 1] + (i & 1)
+
+        return ans      
